68d2b0873a01d31e9ffb18faeca40462d9de0de1
besthor <igbebestor7@gmail.com>
2023-03-26 02:53:25 -0700

    fixing output errors

--- 4-get_precision.c
+++ 4-get_precision.c
@@ -1,4 +1,5 @@
-Copy code
+#include "main.h"
+
 /**
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
@@ -9,30 +10,28 @@ Copy code
  */
 int get_precision(const char *format, int *i, va_list list)
 {
-    int curr_i = *i + 1;
-    int precision = -1;
+	int curr_i = *i + 1;
+	int precision = -1;
 
-    if (format[curr_i] != '.')
-        return (precision);
+	if (format[curr_i] != '.')
+		return (precision);
+	precision = 0;
+	curr_i++;
 
-    precision = 0;
-    curr_i++;
+	if (format[curr_i] == '*')
+	{
+		curr_i++;
+		precision = va_arg(list, int);
+	}
+	else
+	{
+		while (is_digit(format[curr_i]))
+		{
+			precision = precision * 10 + (format[curr_i] - '0');
+			curr_i++;
+		}
+	}
+	*i = curr_i - 1;
 
-    if (format[curr_i] == '*')
-    {
-        curr_i++;
-        precision = va_arg(list, int);
-    }
-    else
-    {
-        while (is_digit(format[curr_i]))
-        {
-            precision = precision * 10 + (format[curr_i] - '0');
-            curr_i++;
-        }
-    }
-
-    *i = curr_i - 1;
-
-    return (precision);
+	return (precision);
 }
