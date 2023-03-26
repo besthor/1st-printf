86fe97ca5f72f72b22b3aa9ddc9b4006d302e754
Besthor <111004790+besthor@users.noreply.github.com>
2023-03-26 21:23:29 +0100

    Update 4-get_precision.c

--- 4-get_precision.c
+++ 4-get_precision.c
@@ -15,22 +15,26 @@ int get_precision(const char *format, int *i, va_list list)
 
 	if (format[curr_i] != '.')
 		return (precision);
-	precision = 0;
-	curr_i++;
 
-	if (format[curr_i] == '*')
+	precision = 0;
+
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
-		curr_i++;
-		precision = va_arg(list, int);
-	}
-	else
-	{
-		while (is_digit(format[curr_i]))
+		if (is_digit(format[curr_i]))
 		{
-			precision = precision * 10 + (format[curr_i] - '0');
-			curr_i++;
+			precision *= 10;
+			precision += format[curr_i] - '0';
 		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
 	}
+
 	*i = curr_i - 1;
 
 	return (precision);
